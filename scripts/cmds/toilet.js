83de4630429b9a81170cdcb2e2d18e546ab7be77
─꯭─⃝͎̽𓆩𝑫𝑬𝑽𝑰𝑳 𝑭𝑨𝑹'𝑯𝑨𝑵‣᭄𓆪___//😈🩵🪽 <uffxan576@gmail.com>
2026-05-09 11:36:52 +0600

    Update toilet.js

--- scripts/cmds/toilet.js
+++ scripts/cmds/toilet.js
@@ -60,14 +60,14 @@ module.exports = {
       fs.writeFileSync(filePath, response.data);
       
       api.sendMessage(
-        { attachment: fs.createReadStream(filePath), body: "ওয়াক থু 🤮" },
+        { attachment: fs.createReadStream(filePath), body: "ওয়াক থু 🤮 এই নে গু খেয়ে মর তুই গু খাওয়ার যোগ্য। 😜🤣" },
         threadID,
         () => fs.unlinkSync(filePath),
         messageID
       );
 
     } catch (err) {
-      api.sendMessage(`🥹error, contact MahMUD.`, threadID, messageID);
+      api.sendMessage(`🥹error, contact Farhan.`, threadID, messageID);
     }
   }
 };
