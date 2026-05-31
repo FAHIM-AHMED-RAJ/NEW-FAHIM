124776196e412c149481e3f08ef52c648a2291ce
FAHIM-AHMED-RAJ <ceofahim650@gmail.com>
2026-05-31 18:49:24 +0600

    Update autoreactunsend.js

--- scripts/cmds/autoreactunsend.js
+++ scripts/cmds/autoreactunsend.js
@@ -1,21 +1,25 @@
 module.exports = {
 	config: {
 		name: "autoreactunsend",
-		version: "1.0",
+		version: "1.1",
 		author: "ChatGPT",
 		role: 0,
-		description: "🐦‍🔥 react dile bot msg unsend korbe",
+		description: "🐦‍🔥 / 🌚 react dile bot msg unsend",
 		category: "box chat"
 	},
 
 	onReaction: async function ({ event, api }) {
 		try {
+
+			const react = ["🐦‍🔥", "🌚"];
+
 			if (
-				event.reaction == "🐦‍🔥" &&
+				react.includes(event.reaction) &&
 				event.userID != api.getCurrentUserID()
 			) {
 				api.unsendMessage(event.messageID);
 			}
+
 		}
 		catch (e) {
 			console.log(e);
